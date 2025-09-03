b86f611dcd9407dbc97f02c24b6019a9661470e5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-09-03 14:02:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+from numpy.random import default_rng as rng
 
-st.title('🎈 App Name')
+changes = list(rng(4).standard_normal(20))
+data = [sum(changes[:i]) for i in range(20)]
+delta = round(data[-1], 2)
 
-st.write('Hello world!')
+row = st.container(horizontal=True)
+with row:
+    st.metric(
+        "Line", 10, delta, chart_data=data, chart_type="line", border=True
+    )
+    st.metric(
+        "Area", 10, delta, chart_data=data, chart_type="area", border=True
+    )
+    st.metric(
+        "Bar", 10, delta, chart_data=data, chart_type="bar", border=True
+    )
