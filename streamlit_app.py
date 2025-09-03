2c77aa2eee7b5ffc2709cd6bf5e4d8650663d7cc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-09-03 14:21:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,38 @@ with row2:
     st.metric(
         "Bar", 20, delta2, chart_data=data2, chart_type="bar", border=True
     )
+
+with st.expander("See Code"):
+    st.code("""
+    changes = list(rng(4).standard_normal(20))
+    data = [sum(changes[:i]) for i in range(20)]
+    delta = round(data[-1], 2)
+    
+    row = st.container(horizontal=True)
+    with row:
+        st.metric(
+            "Line", 10, delta, chart_data=data, chart_type="line", border=True
+        )
+        st.metric(
+            "Area", 10, delta, chart_data=data, chart_type="area", border=True
+        )
+        st.metric(
+            "Bar", 10, delta, chart_data=data, chart_type="bar", border=True
+        )
+    
+    changes2 = list(-rng(4).standard_normal(20))
+    data2 = [sum(changes2[:i]) for i in range(20)]
+    delta2 = round(data2[-1], 2)
+    
+    row2 = st.container(horizontal=True)
+    with row2:
+        st.metric(
+            "Line", 20, delta2, chart_data=data2, chart_type="line", border=True
+        )
+        st.metric(
+            "Area", 20, delta2, chart_data=data2, chart_type="area", border=True
+        )
+        st.metric(
+            "Bar", 20, delta2, chart_data=data2, chart_type="bar", border=True
+        )
+    """)
