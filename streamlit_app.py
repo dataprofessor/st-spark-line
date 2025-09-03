4df66bfd30f512948b9d8e7286bd4dbd2eb3e0bc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-09-03 14:25:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from numpy.random import default_rng as rng
 
+# Displays app title and description
 st.title(":material/show_chart: Show Sparklines")
-
 st.warning("To show trends over time, add sparklines with the `chart_data` parameter.")
 
+# Display toggable code block
 with st.expander(":material/code_blocks: See Code"):
     st.code("""
     changes = list(rng(4).standard_normal(20))
@@ -40,6 +41,7 @@ with st.expander(":material/code_blocks: See Code"):
         )
     """)
 
+# Display metrics with sparklines (positive trend)
 changes = list(rng(4).standard_normal(20))
 data = [sum(changes[:i]) for i in range(20)]
 delta = round(data[-1], 2)
@@ -56,6 +58,7 @@ with row:
         "Bar", 10, delta, chart_data=data, chart_type="bar", border=True
     )
 
+# Display metrics with sparklines (negative trend)
 changes2 = list(-rng(4).standard_normal(20))
 data2 = [sum(changes2[:i]) for i in range(20)]
 delta2 = round(data2[-1], 2)
