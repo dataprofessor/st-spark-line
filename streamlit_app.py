01e23289d9a3ba5dcee0651d7acd43699ea18d26
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-09-04 11:03:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from numpy.random import default_rng as rng
 
+st.set_page_config(
+    page_title="Show Sparklines",
+    page_icon=":material/show_chart:",
+    layout="wide")
+
 # Displays app title and description
 st.title(":material/show_chart: Show Sparklines")
 st.warning("To show trends over time, add sparklines with the `chart_data` parameter for `st.metric()`.")
